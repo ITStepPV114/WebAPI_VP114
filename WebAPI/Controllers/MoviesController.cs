78961dd4af06d232f6aa13a79115325a8782212e
TSShrol <shrolts@gmail.com>
2023-09-13 21:21:18 +0300

    Create Action for Get, Post, Put, Delete

---
+++ WebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Entities;
+using DataAccess.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IRepository<Movie> _repository;
+        public MoviesController(IRepository<Movie> repository )
+        {
+            _repository = repository;
+        }
+
+        [HttpGet] //GET: ~/api/movies
+        //[HttpGet("all")] //GET: ~/api/movies/all
+        //[HttpGet("/movies")] //GET: ~/movies
+        public IActionResult Get() {
+
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("{id}")] 
+        public IActionResult Get([FromRoute] int id)  //default FromQuery   => ~/api/movies?id=2   //FromRoute => ~/api/movies/2
+        { 
+            return Ok(_repository.GetByID(id));
+        }
+        [HttpPost("Create")]
+        public IActionResult Create(Movie movie) {
+            _repository.Insert(movie);
+            _repository.Save();
+            return Ok();
+        }
+
+        [HttpPut("Edit")]
+        public IActionResult Edit(Movie movie)
+        {
+            _repository.Update(movie);
+            _repository.Save();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id) {
+            _repository.Delete(id);
+            _repository.Save();
+            return Ok();
+        }
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,9 @@
+using DataAccess;
 using DataAccess.Data;
+using DataAccess.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+builder.Services.AddSwaggerGen(options =>
+{
+    options.MapType<TimeSpan>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Example = new OpenApiString("00:00:00")
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
