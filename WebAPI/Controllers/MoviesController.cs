260f4b70f62889a98e0196c94226edbdb9e4413b
TSShrol <shrolts@gmail.com>
2023-12-01 12:13:30 +0200

    change access

--- WebAPI/Controllers/MoviesController.cs
+++ WebAPI/Controllers/MoviesController.cs
@@ -17,7 +17,7 @@ namespace WebAPI.Controllers
         }
 
         [HttpGet] //GET: ~/api/movies
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         //[HttpGet("all")] //GET: ~/api/movies/all
         //[HttpGet("/movies")] //GET: ~/movies
         public async Task<IActionResult> Get()
